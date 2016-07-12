a18322a42ebdc1511acc34fb1beadcf3f9b38d07
Stan Zhang <stan.zhang2@gmail.com>
2016-07-12 01:55:22 -0400

    02-ADTs parse

---
+++ 02-ADTs/LogAnalysis.hs
@@ -0,0 +1,36 @@
+module LogAnalysis where
+
+import Log
+
+maybeRead :: Read a => String -> Maybe a
+maybeRead str = case reads str of
+                  [(a, "")] -> Just a
+                  _ -> Nothing
+
+maybeApply :: (a -> b) -> Maybe a -> Maybe b
+maybeApply f = maybe Nothing $ \x -> Just $ f x
+
+maybeApply2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+maybeApply2 _ Nothing _ = Nothing
+maybeApply2 _ _ Nothing = Nothing
+maybeApply2 f (Just a) (Just b) = Just $ f a b
+
+parseMessageHeader :: [String] -> Maybe (MessageType, TimeStamp, [String])
+parseMessageHeader (mt : x : ys)
+  | mt == "I" = maybeApply (\ts -> (Info, ts, ys)) $ maybeRead x
+  | mt == "W" = maybeApply (\ts -> (Warning, ts, ys)) $ maybeRead x
+  | mt == "E" =
+      case ys of
+        (t : rest) -> maybeApply2 (\c ts -> (Error c, ts, rest)) (maybeRead x) (maybeRead t)
+        _ -> Nothing
+  | otherwise = Nothing
+
+parseMessage :: String -> LogMessage
+parseMessage s =
+    case parseMessageHeader . words $ s of
+      Just (mt, ts, rest) -> LogMessage mt ts (unwords rest)
+      _ -> Unknown s
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
+

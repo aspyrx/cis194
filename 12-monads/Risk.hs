8c12f35c50f64d0a112bb3c83f5dca00aee45c83
Stan Zhang <stan.zhang2@gmail.com>
2016-07-16 23:32:35 -0400

    12-monads finished

--- 12-monads/Risk.hs
+++ 12-monads/Risk.hs
@@ -2,12 +2,15 @@
 
 module Risk where
 
+import Control.Applicative
+import Control.Monad
 import Control.Monad.Random
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
@@ -26,3 +29,29 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+    deriving (Show)
+
+roll :: Int -> Rand StdGen [DieValue]
+roll n = sortBy (flip compare) <$> replicateM n die
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle b@(Battlefield attack defend) =
+    let dec n = max 0 (n - 1)
+        f True (Battlefield a d) = Battlefield a (dec d)
+        f False (Battlefield a d) = Battlefield (dec a) d
+     in do att <- (roll $ max attack 3)
+           def <- (roll $ max defend 2)
+           return $ foldr f b $ zipWith (>) att def
+
+invade :: Battlefield -> Rand StdGen Battlefield
+invade b@(Battlefield attack defend) = if attack > 1 && defend > 0
+                                          then do bt <- battle b
+                                                  invade bt
+                                          else return b
+
+successProb :: Battlefield -> Rand StdGen Double
+successProb b = let wins :: Battlefield -> Double -> Double
+                    wins (Battlefield _ defend) = if defend > 0 then id else (+ 1)
+                 in do bs <- replicateM 1000 (invade b)
+                       return $ foldr wins 0 bs / 1000
+
